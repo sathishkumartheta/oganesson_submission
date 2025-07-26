d1fece508867a1200e5593be96fdff0f4367b8aa
sathishkumartheta <sathishkumartheta@gmail.com>
2025-07-26 22:29:27 +0000

    deployed and working

--- mm_agent/agent.py
+++ mm_agent/agent.py
@@ -18,33 +18,24 @@ mood_agent=Agent(
     You are an agent to guage the mood of different localities from Bengaluru
     You have access to the following tools : 
     reddit_search, toi_search and google_search
-    "Majestic",
-    "KR Market",
-    "MG Road",
-    "Shivajinagar",
-    "Richmond Town",
-    "Electronic City",
-    "Whitefield",
-    "Marathahalli",
-    "Outer Ring Road",
-    "Koramangala",
-    "HSR Layout",
-    "Indiranagar",
-    "Jayanagar",
-    "Basavanagudi",
-    "Banashankari",
-    "Malleshwaram",
-    "Yelahanka",
-    "Hebbal",
-    "Silk Board",
-    "Bellandur",
-    "Kengeri",
-    "Bommanahalli",
-    "BTM Layout"
+    1. Majestic  
+    2. MG Road  
+    3. Electronic City  
+    4. Whitefield  
+    5. Koramangala  
+    6. Indiranagar  
+    7. Jayanagar  
+    8. Hebbal  
+    9. Silk Board
+
     For each of these localities, you can use the tools multiple times to guage the mood of the user.
     Give out a single string output that contains the description of the mood for each of the locality
     the string output should clearly mention the locality name and  its mood.
-    Ignore the localities, for which you cannot access the mood
+    Ignore the localities, for which you cannot access the mood..
+    You need to definitely try both tools for each of the above cities atleast once
+    and not more than thrice
+    You need to output the mood for all the places. for localities for which you cannot guage the mood
+    output that you cannot guage the mood.. but proceed with guaging the mood for other localities
     """,
     tools=[reddit_search,toi_search]
 )
